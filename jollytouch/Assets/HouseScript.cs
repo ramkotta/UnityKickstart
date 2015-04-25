c65a1b171a515c6a5b3c7ccac2409a7f5f222c2c
Ram Kotta <ramkotta@wireless-10-147-131-77.public.utexas.edu>
2015-04-25 16:03:41 -0500

    Created basic House generator script

--- jollytouch/Assets/HouseScript.cs
+++ jollytouch/Assets/HouseScript.cs
@@ -1,11 +1,60 @@
 ﻿using UnityEngine;
-using System.Collections;
+//using System.Collections;
+using System;
 
 public class HouseScript : MonoBehaviour {
+
+	int initNumBees;
+	int numBees;
+
+	System.Random rand;
 
 	// Use this for initialization
 	void Start () {
 		//this is where we generate info for the house.
+		rand = new System.Random ();
+
+		double amountIncr = 0;
+		double amountDecr = 0;
+
+		numBees = rand.Next (10, 20);
+		Debug.Log ("Initial number of bees is: " + numBees);
+
+		if (numBees != 0) {
+			int chanceBugSpray = rand.Next(1,4);
+			Debug.Log ("Bug spray number is: " + chanceBugSpray);
+			if (chanceBugSpray == 1) {
+				amountDecr += 0.5;
+			}
+			int chanceFlowers = rand.Next(1,4);
+			Debug.Log ("Flower number is: " + chanceFlowers);
+			if (chanceFlowers == 1) {
+				amountIncr += 0.1;
+			}
+			int chanceHoney = rand.Next(1,5);
+			Debug.Log ("Honey number is: " + chanceHoney);
+			if (chanceHoney == 1) {
+				chanceHoney = rand.Next(1,4);
+				Debug.Log ("Second Honey number is: " + chanceHoney);
+				if (chanceHoney == 1) {
+					amountIncr += 0.2;
+				} else if (chanceHoney == 2) {
+					amountIncr += 0.4;
+				} else if (chanceHoney == 3) {
+					amountIncr += 0.6;
+				}
+			}
+			int chanceHive = rand.Next(1,11);
+			Debug.Log ("Hive number is: " + chanceHive);
+			if (chanceHive == 1) {
+				amountIncr += 2;
+			}
+			if (amountIncr != 0) {
+				numBees = (int) Math.Round(numBees - amountDecr*numBees + amountIncr*numBees);
+			}
+		}
+		Debug.Log ("Final Number of bees is: " + numBees);
+
 	}
 	
 	// Update is called once per frame

e4aa1b6318ebd6a8d783b22fbbfee45801cf4844
Kevin Sun <kevin.sun96@gmail.com>
2015-04-26 10:26:30 -0500

    Balancing and UI

--- jollytouch/Assets/Scripts/BeeSystem.cs
+++ jollytouch/Assets/Scripts/BeeSystem.cs
@@ -4,34 +4,97 @@ using System.Collections;
 
 public class BeeSystem : MonoBehaviour {
 	
+	public GameObject[] houses;
+	public Texture2D menu;
+
+	bool showNotification = false;
+	bool showingWindow = false;
+
+	string message = "";
+
 	int totalBees;
-	int idleBees;
 	int attackingBees;
-	
+	int housesConquered;
+	int difficulty;
+	int timeNotification = 2;
+	float timeElapsed = 0;
+
+	int menu_width = 662;
+	int menu_height = 371;
 	// Use this for initialization
 	void Start () {
-		totalBees = 20;
-		idleBees = totalBees;
+		totalBees = 30;
 		attackingBees = 0;
+		housesConquered = 0;
+		difficulty = 0;
 	}
 	
-	void setAttackingBees(int attackers) {
-		if (hasIdleBees ()) {
-			attackingBees += attackers;
-			idleBees = totalBees - attackingBees;
-		}  else {
-			Debug.Log("There are no bees left to attack with.");
+	public bool setAttackingBees(int attackers) {
+		if (totalBees >= attackers) {
+			attackingBees = attackers;
+			return true;
+		}
+		Debug.Log("Not enough bees");
+		return false;
+	}
+
+	public void addZombees(int numZombees, int id){
+		int beeDiff = numZombees - attackingBees;
+		totalBees += beeDiff;
+		message = "You sent in " + attackingBees + " zombees";
+		if (beeDiff > 0){
+			message = " and brought back " + beeDiff + " new zom-bees!";
+			houses[id].GetComponent<HouseScript>().createHouse(difficulty);
+		} else if (beeDiff < 0){
+			message = " but only  " + -1 * beeDiff + " came back.";
+		}
+		Debug.Log(totalBees + ": " + numZombees + ": " + message);
+		timeElapsed = 0;
+		showNotification = true;
+		if (numZombees >= 0) {
+			housesConquered++;
+			if (housesConquered % 3 == 0){
+				difficulty++;
+				for (int i = 0; i < houses.Length; i++){
+					houses[i].GetComponent<HouseScript>().createHouse(difficulty);
+				}
+				Debug.Log(difficulty);
+			}
 		}
 	}
-	
-	int getAttackingBees() {
-		return attackingBees;
+
+	public bool showMenu(bool mouseOver, bool attackSetup){
+		bool result = !showingWindow && (mouseOver || attackSetup);
+		if (showNotification) showNotification = !(showNotification || result);
+		showingWindow = result;
+		Debug.Log(result);
+		return result;
 	}
 	
-	bool hasIdleBees() {
-		return idleBees != 0;
+	void OnGUI(){
+		if (showNotification){
+			timeElapsed += Time.deltaTime;
+			if (timeElapsed >= 4){
+				showNotification = false;
+			}
+			GUI.BeginGroup(new Rect(10, 10, menu_width / 3, menu_height / 4));
+			GUI.DrawTexture(new Rect(0, 0, menu_width / 3, menu_height / 4), menu);
+			GUI.color = Color.black;
+			GUI.Label(new Rect(10, 10, 200, 30), message);
+			GUI.color = Color.white;
+			GUI.EndGroup();
+		}
+		int screenW = Screen.width;
+		int screenH = Screen.height;
+		GUI.BeginGroup(new Rect(screenW * 5/7, screenH * 5/6, menu_width/ 4, menu_height / 5));
+		GUI.DrawTexture(new Rect(0, 0, menu_width / 4, menu_height / 5), menu);
+		GUI.color = Color.black;
+		GUI.Label(new Rect(15, 10, 200, 30), "Zom-bees you control");
+		GUI.Label(new Rect(15, 30, 50, 30), "" + totalBees);
+		GUI.color = Color.white;
+		GUI.EndGroup();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
--- jollytouch/Assets/Scripts/HouseScript.cs
+++ jollytouch/Assets/Scripts/HouseScript.cs
@@ -11,6 +11,7 @@ public class HouseScript : MonoBehaviour {
 	System.Random rand;
 	public int id;
 	public Texture2D infoMenu;
+	public GameObject parent;
 
 	bool mouseOver = false;
 	bool attackSetup = false;
@@ -27,57 +28,56 @@ public class HouseScript : MonoBehaviour {
 	void Start () {
 		//this is where we generate info for the house.
 		rand = new System.Random ();
-
+		createHouse(0);
+	}
+	
+	public void createHouse(int difficulty) {
+		bugSprayStatus = null;
+		flowerStatus = null;
+		honeyStatus = null;
+		hiveStatus = null;
 		double amountIncr = 0;
 		double amountDecr = 0;
 
 		defendingBees = rand.Next (10, 20);
 		attackingBees = 0;
-		Debug.Log ("Initial number of bees is: " + defendingBees);
 
 		if (defendingBees != 0) {
-			int chanceBugSpray = rand.Next(1,4);
-			Debug.Log ("Bug spray number is: " + chanceBugSpray);
-			if (chanceBugSpray == 1) {
+			int chanceBugSpray = rand.Next(40 + difficulty);
+			if (chanceBugSpray <= 10) {
 				amountDecr += 0.5;
 				bugSprayStatus = "Bug Spray";
 			}
-			int chanceFlowers = rand.Next(1,4);
-			Debug.Log ("Flower number is: " + chanceFlowers);
-			if (chanceFlowers == 1) {
+			int chanceFlowers = rand.Next(40 - difficulty);
+			if (chanceFlowers <= 10) {
 				amountIncr += 0.1;
 				flowerStatus = "Flowers";
 			}
-			int chanceHoney = rand.Next(1,5);
-			Debug.Log ("Honey number is: " + chanceHoney);
-			if (chanceHoney == 1) {
-				chanceHoney = rand.Next(1,4);
-				Debug.Log ("Second Honey number is: " + chanceHoney);
-				if (chanceHoney == 1) {
+			int chanceHoney = rand.Next(50 - difficulty);
+			if (chanceHoney <= 10) {
+				chanceHoney = rand.Next(40 - difficulty);
+				if (chanceHoney <= 10) {
 					amountIncr += 0.2;
 					honeyStatus = "Slight scent of honey";
-				} else if (chanceHoney == 2) {
-					amountIncr += 0.4;
+				} else if (chanceHoney <= 20) {
+					amountIncr += 0.3;
 					honeyStatus = "Distinct scent of honey";
-				} else if (chanceHoney == 3) {
-					amountIncr += 0.6;
+				} else if (chanceHoney <= 30) {
+					amountIncr += 0.4;
 					honeyStatus = "Strong scent of honey";
 				}
 			}
-			int chanceHive = rand.Next(1,11);
-			Debug.Log ("Hive number is: " + chanceHive);
-			if (chanceHive == 1) {
-				amountIncr += 2;
+			int chanceHive = rand.Next(100 - difficulty);
+			if (chanceHive <= 10) {
+				amountIncr += 1;
 				hiveStatus = "Bee hive present";
 			}
 			if (amountIncr != 0) {
 				defendingBees = (int) Math.Round(defendingBees - amountDecr*defendingBees + amountIncr*defendingBees);
 			}
 		}
-		Debug.Log ("Final Number of bees is: " + defendingBees);
-
+		Debug.Log (id + " bees: " + defendingBees);
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		//may not use?
@@ -101,16 +101,15 @@ public class HouseScript : MonoBehaviour {
 
 	void OnGUI(){
 		if (mouseOver || attackSetup){
-			int col = id % 3;
-			int row = id / 3;
-			float pivot_x = col * Screen.width / 3f;
-			float pivot_y = row * Screen.height / 3f;
+			int col = id % 3 + 1;
+			int row = id / 3 + 1;
+			float pivot_x = col * Screen.width / 5f;
+			float pivot_y = row * Screen.height / 5f;
 			//GUI.BeginGroup(new Rect(400 + (int)this.transform.position.x * 10, 100 + (int)this.transform.position.y * 10, menu_width, menu_height));
 			GUI.BeginGroup(new Rect((int)pivot_x, (int)pivot_y, menu_width, menu_height));
 			GUI.DrawTexture(new Rect(0, 0, menu_width / 4, menu_height / 2), infoMenu);
 			GUI.color = Color.black;
-			GUI.Label(new Rect(20, 10, 100, 20), "House Info:");
-			int text_y = 30;
+			int text_y = 10;
 			if (bugSprayStatus != null) {
 				GUI.Label(new Rect(20, text_y, 150, 20), bugSprayStatus);
 				text_y += 20;
@@ -125,17 +124,34 @@ public class HouseScript : MonoBehaviour {
 			}
 			if (hiveStatus != null) {
 				GUI.Label(new Rect(20, text_y, 150, 20), hiveStatus);
+				text_y += 20;
+			}
+			if (text_y == 10){
+				GUI.Label(new Rect(20, text_y, 150, 20), "No notable info");
 			}
 			if (attackSetup){
 				GUI.Label(new Rect(20, 110, 150, 20), "Number of beees to send in:");
 				strNumBees = GUI.TextField(new Rect(20, 130, 80, 20), strNumBees);
-				if (GUI.Button(new Rect(20, 150, 100, 20), "Attack!")){			
-					int defendingBees;
-					if (Int32.TryParse(strNumBees, out defendingBees)){
-						Debug.Log(defendingBees);
+				if (GUI.Button(new Rect(20, 150, 50, 20), "Attack!")){			
+					if (Int32.TryParse(strNumBees, out attackingBees)){
+						if (parent.GetComponent<BeeSystem>().setAttackingBees(attackingBees)){
+							if (attackingBees > defendingBees){
+								attackingBees /= 3;
+								attackingBees += defendingBees;
+							} else if (attackingBees == defendingBees){
+								//stuff
+							} else {
+								attackingBees *= -1;
+							}
+							parent.GetComponent<BeeSystem>().addZombees(attackingBees, id);
+						}
 					} else {
 						Debug.Log("Das it mane");
 					}
+					attackSetup = false;
+					mouseOver = false;
+				}
+				if (GUI.Button(new Rect(100, 150, 50, 20), "Cancel")){
 					attackSetup = false;
 					mouseOver = false;
 				}

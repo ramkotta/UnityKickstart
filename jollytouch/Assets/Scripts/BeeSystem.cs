ccd3603bafdc3fd54e8220fda97455fc7b7a7249
Ram Kotta <ramkotta@wireless-10-147-131-77.public.utexas.edu>
2015-04-25 19:10:14 -0500

    Actually added BeeSystem

---
+++ jollytouch/Assets/Scripts/BeeSystem.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class BeeSystem : MonoBehaviour {
+	
+	int totalBees;
+	int idleBees;
+	int attackingBees;
+	
+	// Use this for initialization
+	void Start () {
+		totalBees = 20;
+		idleBees = totalBees;
+		attackingBees = 0;
+	}
+	
+	void setAttackingBees(int attackers) {
+		if (hasIdleBees ()) {
+			attackingBees += attackers;
+			idleBees = totalBees - attackingBees;
+		}  else {
+			Debug.Log("There are no bees left to attack with.");
+		}
+	}
+	
+	int getAttackingBees() {
+		return attackingBees;
+	}
+	
+	bool hasIdleBees() {
+		return idleBees != 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
